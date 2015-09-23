9b0a940c2b5ac981fb27a771a0506da122f21755
Arnaud Dupenne <adupenne@gmail.fr>
2015-09-23 14:30:42 +0200

    ajout login.html

---
+++ src/main/webapp/app/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name projectsApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the projectsApp
+ */
+angular.module('projectsApp')
+  .controller('LoginCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
