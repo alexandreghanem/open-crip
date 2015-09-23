23542272a20e3dae674e648908f36c1cac0311ea
adrienvoss <adrienvoss@gmail.com>
2015-09-24 01:03:01 +0200

    Add geolocation view

--- src/main/webapp/app/scripts/controllers/search_map.js
+++ src/main/webapp/app/scripts/controllers/search_map.js
@@ -11,10 +11,27 @@
 
 
 angular.module('projectsApp')
-  .controller('SearchMapCtrl', function () {	
+  .controller('SearchMapCtrl', function ($scope, $geolocation, $compile) {	
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    
+    $scope.$geolocation = $geolocation;
+
+    $geolocation.getCurrentPosition().then(function(location) {
+      $scope.location = location
+    });
+
+    // regular updates
+    $geolocation.watchPosition({ 
+      timeout: 60000,
+      maximumAge: 2,
+      enableHighAccuracy: true
+    });
+    $scope.coords = $geolocation.position.coords; // this is regularly updated
+    $scope.error = $geolocation.position.error; // this becomes truthy, and has 'code' and 'message' if an error occurs
   });
+
+
