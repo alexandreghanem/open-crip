526a2b5c20bf8c230cfffe19f328a7344956580b
Arnaud Dupenne <adupenne@gmail.fr>
2015-09-24 12:50:29 +0200

    ajout login.html

--- src/main/java/fr/aston/opencrip/web/bean/AuthentificationBean.java
+++ src/main/java/fr/aston/opencrip/web/bean/AuthentificationBean.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import javax.servlet.http.HttpServletRequest;
 
+
 public class AuthentificationBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
